d6e5096396e14b58a23dc25825e103752e2c4391
Memetelve <memetelve@gmail.com>
2025-11-20 21:56:31 +0100

    feat: initialize repo

---
+++ src/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Request
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+
+from src.core.settings import Settings
+from src.v1.auth.auth_router import auth_router
+
+
+settings = Settings()
+
+
+app = FastAPI(title="PYTA API")
+
+app.add_middleware(
+    CORSMiddleware,
+)
+
+app.include_router(auth_router)
+
+
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(
+    request: Request, exc: ValidationError
+) -> JSONResponse:
+    errors = exc.errors()[0]
+
+    details = {
+        "loc": errors["loc"],
+        "msg": errors["msg"],
+        "type": errors["type"],
+    }
+
+    return JSONResponse(status_code=422, content={"detail": details})
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "src.main:app", host="0.0.0.0", port=settings.port, reload=settings.debug
+    )
